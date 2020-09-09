a89172e6206ad77ab52166933b1c3d1c74334e78
unknown <leandroefernandapaiva@gmail.com>
2020-09-09 13:32:16 -0300

    LaçoCondicional

---
+++ Ex3CategoriaEscolar/Ex3CategoriaEscolar.java
@@ -0,0 +1,32 @@
+package Ex3CategoriaEscolar;
+
+import java.util.Scanner;
+
+public class Ex3CategoriaEscolar {
+
+	public static void main(String[] args) {
+		/*Faça um programa que receba a idade de uma pessoa e mostre na saída em qual categoria ela se encontra:
+			·                     10-14 infantil
+			·                     15-17 juvenil
+			·                     18-25 adulto*/
+		int idade;
+		Scanner leia = new Scanner(System.in);
+		System.out.println("Digite sua idade: ");
+		idade = leia.nextInt();
+		if (idade>=10 && idade<=14)
+		{
+		System.out.printf("Sua categoria é infantil, %d anos", idade );
+		}
+		else if (idade>=15 && idade<=17)
+		{
+	    System.out.printf("Sua categoria é juvenil, %d anos", idade);
+		}
+		else if (idade>=18 && idade<=25)
+		{
+		System.out.printf("Sua categoria é adulto, %d anos", idade);	
+		}
+
+
+	}
+
+}
