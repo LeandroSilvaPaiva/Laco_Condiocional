76b891794e9176749148e893bca01cc7a21c280f
unknown <leandroefernandapaiva@gmail.com>
2020-09-16 00:05:38 -0300

    POO

--- PooPaciente/src/DadosFuncionario/DadosFuncionario.java
+++ PooPaciente/src/DadosFuncionario/DadosFuncionario.java
@@ -13,11 +13,11 @@ public class DadosFuncionario {
 	{
 		if(sexoFuncionario=='M')
 		{	
-			System.out.printf("\nO nome do funcinario é %s, função: %s tem %d anos e ele é do sexo Masculino.", nomeFuncionario,funcaoFuncionario, idadeFuncionario);
+			System.out.printf("\nO nome do funcinario é %s, função: %s, tem %d anos e ele é do sexo Masculino.", nomeFuncionario,funcaoFuncionario, idadeFuncionario);
 		}
 		else if(sexoFuncionario=='F')
 		{	
-			System.out.printf("\nO nome da funcionario é %s, função: %s tem %d anos e ela é do sexo Feminina.", nomeFuncionario, funcaoFuncionario, idadeFuncionario);
+			System.out.printf("\nO nome da funcionario é %s, função: %s, tem %d anos e ela é do sexo Feminino.", nomeFuncionario, funcaoFuncionario, idadeFuncionario);
 		}
 	}
 	public void horas()
@@ -28,11 +28,11 @@ public class DadosFuncionario {
 		}
 		else if(horasTrabalhadas < 44)
 		{	
-			System.out.printf("\nVocê deve %.2f/44 horas semanais ", (horasTrabalhadas - 44));
+			System.out.printf("\nVocê deve %.2f horas", (44 - horasTrabalhadas));
 		}
 		else if(horasTrabalhadas > 44)
 		{	
-			System.out.printf("\nVocê tem banco de %.2f/44 horas semanais", horasTrabalhadas);
+			System.out.printf("\nVocê tem banco de %.2f horas", (horasTrabalhadas - 44));
 		}
 	}
 
